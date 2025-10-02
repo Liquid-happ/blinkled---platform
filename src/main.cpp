9dcc04d7efa376cf547bc5157e076d0044dec94b
Liquid-happ <duongbaquyet_t67@hus.edu.vn>
2025-10-02 22:04:06 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,39 +1,19 @@
 #include <Arduino.h>
-#include <OneButton.h>
 
-#define LED_PIN 2
-#define BTN_PIN 4
-
-OneButton button(BTN_PIN, true); // true = sử dụng INPUT_PULLUP
-
-bool ledState = false;   // trạng thái bật/tắt LED
-bool blinking = false;   // trạng thái nháy LED
-
-// Single click: ON/OFF LED
-void toggleLed() {
-  blinking = false;   // nếu đang nháy thì dừng nháy
-  ledState = !ledState;
-  digitalWrite(LED_PIN, ledState ? HIGH : LOW);
-}
-
-// Double click: chuyển sang chế độ nháy LED
-void toggleBlinking() {
-  blinking = !blinking;
-}
+#define LED_BUILTIN 2   
+#define LED_EXT     4   
 
 void setup() {
-  pinMode(LED_PIN, OUTPUT);
-  button.attachClick(toggleLed);          // single click
-  button.attachDoubleClick(toggleBlinking); // double click
+  pinMode(LED_BUILTIN, OUTPUT);
+  pinMode(LED_EXT, OUTPUT);
 }
 
 void loop() {
-  button.tick(); // xử lý button mỗi vòng lặp
+  digitalWrite(LED_BUILTIN, HIGH);
+  digitalWrite(LED_EXT, HIGH);
+  delay(1000);
 
-  if (blinking) {
-    digitalWrite(LED_PIN, HIGH);
-    delay(200);
-    digitalWrite(LED_PIN, LOW);
-    delay(200);
-  }
+  digitalWrite(LED_BUILTIN, LOW);
+  digitalWrite(LED_EXT, LOW);
+  delay(1000);
 }
